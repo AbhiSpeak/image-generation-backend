dfbd8dd1fd580b49faf2e50ca760ccfec7889ada
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-05-21 13:01:29 +0530

    added favourite field

--- mongodb/models/post.js
+++ mongodb/models/post.js
@@ -1,12 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Post = new mongoose.Schema({
-    name: {type: String, required: true},
-    prompt: {type: String, required: true},
-    photo: {type: String, required: true},
+  name: { type: String, required: true },
+  prompt: { type: String, required: true },
+  photo: { type: String, required: true },
+  favourite:{
+    type: String,
+    required: true,
+    default: false
+  }
 });
 
-
-const PostSchema = mongoose.model('Post', Post);
+const PostSchema = mongoose.model("Post", Post);
 
 export default PostSchema;
